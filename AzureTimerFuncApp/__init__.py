471a9c70303ee961324cbd3dfc0af17d02e01d41
Tom Setliffe <tom@TMacBookPro.local>
2020-07-25 08:52:49 -0700

    updated - working

--- AzureTimerFuncApp/__init__.py
+++ AzureTimerFuncApp/__init__.py
@@ -2,6 +2,8 @@ import datetime
 import json
 import logging
 import subprocess
+import time
+import re
 
 import azure.functions as func
 import gspread
@@ -19,16 +21,17 @@ except:
 COSMOS_URL = Secrets.cosmos_url
 COSMOS_KEY = Secrets.cosmos_key
 SENDGRID_KEY = Secrets.sendgrid_key
+SENDGRID_TEMPLATE_IDS = Secrets.sendgrid_template_ids
 CLIENT = CosmosClient(COSMOS_URL, COSMOS_KEY)
 QUERY_EMAIL = Secrets.query_email
 TEST_FROM_EMAIL = Secrets.test_from_email
 TEST_TO_EMAIL = Secrets.test_to_email
 GSHEET_CREDS = Secrets.gsheet_creds
 GSHEET_NAME = Secrets.gsheet_name
-HOLIDAYS_2020 = {'test_holiday': '07/29/2020', 'thanksgiving': '11/26/2020', 'christmas': '12/25/2020'}
-HOLIDAYS_2021 = {"valentine_day": '02/14/2021', "assistant_day": '04/21/2021', "mother_day": '05/09/2021', 'thanksgiving': '11/25/2021', 'christmas': '12/25/2021'}
-HOLIDAYS_2022 = {"valentine_day": '02/14/2022', "assistant_day": '04/27/2022', "mother_day": '05/08/2022', 'thanksgiving': '11/24/2022', 'christmas': '12/25/2022'}
-HOLIDAYS_2023 = {"valentine_day": '02/14/2023', "assistant_day": '04/26/2023', "mother_day": '05/14/2023', 'thanksgiving': '11/23/2023', 'christmas': '12/25/2023'}
+HOLIDAYS_2020 = {"Valentine's Day": '02/14/2020', "Mother's Day": '5/27/2020', "Professional Assistant's Day": '04/21/2020','Test Holiday': '07/29/2020', 'Thanksgiving': '11/26/2020', 'Christmas': '12/25/2020'}
+HOLIDAYS_2021 = {"Valentine's Day": '02/14/2021', "Professional Assistant's Day": '04/21/2021', "Mother's Day": '05/09/2021', 'Thanksgiving': '11/25/2021', 'Christmas': '12/25/2021'}
+HOLIDAYS_2022 = {"Valentine's Day": '02/14/2022', "Professional Assistant's Day": '04/27/2022', "Mother's Day": '05/08/2022', 'Thanksgiving': '11/24/2022', 'Christmas': '12/25/2022'}
+HOLIDAYS_2023 = {"Valentine's Day": '02/14/2023', "Professional Assistant's Day": '04/26/2023', "Mother's Day": '05/14/2023', 'Thanksgiving': '11/23/2023', 'Christmas': '12/25/2023'}
 DATABASE_NAME= "Contacts"
 CONTAINER_NAME = "Contacts"
 DATABASE = CLIENT.get_database_client(DATABASE_NAME)
@@ -99,13 +102,12 @@ def reminder_date_check(special_date):
 
 def get_new_customer_reminders():
     documents = list(CONTAINER.read_all_items(max_item_count=8000))
-    print('Found {0} items'.format(item_list.__len__()))
+    print('Found {0} items'.format(documents.__len__()))
     birthday_reminders = {}
     ann_reminders = {}
     holiday_reminders = {}
     pattern = re.compile('(\d+)\/(\d+)\/(\d+)')
     for cust_doc in documents:
-        # print(cust_doc)
         try:
             birthday_date = cust_doc['birthday_date']
             birthday_name = cust_doc['birthday_name']
@@ -122,19 +124,20 @@ def get_new_customer_reminders():
             pass
         try:
             holidays = cust_doc['holidays']
-            for holiday in holidays:
-                if reminder_date_check(holiday):
-                    holiday_reminders[cust_doc['email']] = holiday
         except KeyError:
-            pass
+            continue
+        if cust_doc['holidays']:
+            if isinstance(holidays, str):
+                    holidays = holidays.split(',')
+                    holidays = [x.strip() for x in holidays]
+            for holiday in holidays:
+                try:
+                    holiday_date = HOLIDAYS_2020[holiday]
+                except KeyError:
+                    continue
+                if reminder_date_check(holiday_date):
+                    holiday_reminders[cust_doc['email']] = holiday
     return birthday_reminders, ann_reminders, holiday_reminders
-
-def cosmos_query_items(email_address):
-    for item in CONTAINER.query_items(
-        query="SELECT * FROM Contacts p WHERE p.email = '{}'".format(query_email),
-        enable_cross_partition_query=True,
-    ):
-        return(json.dumps(item, indent=True))
 
 def email_customers(from_address, to_address, template_id, special_name=None):
     curl_cmd = """
@@ -159,7 +162,7 @@ def email_customers(from_address, to_address, template_id, special_name=None):
         ],
         "template_id":"{}"
         }}'
-        """.format(SENDGRID_KEY, from_address, to_address, template_id, special_name)
+        """.format(SENDGRID_KEY, from_address, to_address, special_name, template_id)
     subprocess.check_output(curl_cmd, shell=True, universal_newlines=True)
 
 
@@ -171,23 +174,25 @@ def main(mytimer: func.TimerRequest) -> None:
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
 
-    bday_template = sendgrid_template_ids['bday']
-    mothers_day_template = sendgrid_template_ids['mothers']
-    valentines_day_template = sendgrid_template_ids['valentines']
-    thanksgiving_template = sendgrid_template_ids['thanksgiving']
-    xmas_template = sendgrid_template_ids['xmas']
-    assistant_day_template = sendgrid_template_ids['assistant']
-    anniversary_template = sendgrid_template_ids['anniversary']
+    bday_template = SENDGRID_TEMPLATE_IDS['bday']
+    mothers_day_template = SENDGRID_TEMPLATE_IDS['mothers']
+    valentines_day_template = SENDGRID_TEMPLATE_IDS['valentines']
+    thanksgiving_template = SENDGRID_TEMPLATE_IDS['thanksgiving']
+    xmas_template = SENDGRID_TEMPLATE_IDS['xmas']
+    assistant_day_template = SENDGRID_TEMPLATE_IDS['assistant']
+    anniversary_template = SENDGRID_TEMPLATE_IDS['anniversary']
     customer_list = gsheet_export(GSHEET_CREDS, GSHEET_NAME)
+
     cosmos_import(customer_list)
+    time.sleep(10)
     birthday_reminders, ann_reminders, holiday_reminders = get_new_customer_reminders()
     from_address = TEST_FROM_EMAIL
     if birthday_reminders:
         for k,v in birthday_reminders.items():
-            email_customers(from_address, k, bday_template, k[v][0])
+            email_customers(from_address, k, bday_template, v[0])
     if ann_reminders:
         for k,v in ann_reminders.items():
-            email_customers(from_address, k, anniversary_template, k[v][0])
+            email_customers(from_address, k, anniversary_template, v[0])
     if holiday_reminders:
         for k,v in holiday_reminders.items():
             if v == 'Thanksgiving':
@@ -199,5 +204,7 @@ def main(mytimer: func.TimerRequest) -> None:
             if v == "Professional Assistant's Day":
                 email_customers(from_address, k, assistant_day_template)
             if v == "Valentine's Day":
+                email_customers(from_address, k, valentines_day_template)
+            if v == "Test Holiday":
                 email_customers(from_address, k, valentines_day_template)
 
